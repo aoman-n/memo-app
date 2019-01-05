94d67ddd72e7b057326f77bc0976042808b61d4a
aoba <ipbdipbd00@gmail.com>
2019-01-05 20:29:42 +0900

    Create Login and Signup Screen

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import Appbar from './src/components/Appbar';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
 import MemoListScreen from './src/screens/MemoListScreen';
 import MemoEditScreen from './src/screens/MemoEditScreen';
+import LoginScreen from './src/screens/LoginScreen';
+import SignupScreen from './src/screens/SignupScreen';
 
 export default class App extends React.Component {
   render() {
@@ -13,7 +15,9 @@ export default class App extends React.Component {
         <Appbar />
         {/* <MemoDetailScreen /> */}
         {/* <MemoListScreen /> */}
-        <MemoEditScreen />
+        {/* <MemoEditScreen /> */}
+        {/* <LoginScreen /> */}
+        <SignupScreen />
       </View>
     );
   }
